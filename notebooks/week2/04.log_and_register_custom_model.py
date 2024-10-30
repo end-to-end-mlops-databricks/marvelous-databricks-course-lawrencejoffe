91d30724f68323bab022a03f216a957a7f1a500b
Lawrence Joffe <lawrence.joffe@heineken.com>
2024-10-30 13:19:20 +0000

    week 4 done

--- notebooks/week2/04.log_and_register_custom_model.py
+++ notebooks/week2/04.log_and_register_custom_model.py
@@ -1,14 +1,19 @@
 # Databricks notebook source
+
+!pip install /Volumes/heiaepgah71pwedmld01001/lj_loan_prediction/packages/mlops_with_databricks-0.0.1-py3-none-any.whl --force-reinstall
+
+
+# COMMAND ----------
+
 import mlflow
 import numpy as np
 import pandas as pd
 from pyspark.sql import SparkSession
 from mlflow.models import infer_signature
-from house_price.data_processor import ProjectConfig
+from loan_prediction.config import ProjectConfig
 import json
 from mlflow import MlflowClient
 from mlflow.utils.environment import _mlflow_conda_env
-from house_price.utils import adjust_predictions
 
 mlflow.set_registry_uri("databricks-uc")
 mlflow.set_tracking_uri("databricks")
@@ -16,7 +21,7 @@ client = MlflowClient()
 
 # COMMAND ----------
 
-config = ProjectConfig.from_yaml(config_path="../../project_config.yml")
+config = ProjectConfig.from_yaml(config_path="../../config/config.yml")
 
 # Extract configuration details
 num_features = config.num_features
@@ -32,8 +37,15 @@ spark = SparkSession.builder.getOrCreate()
 
 # COMMAND ----------
 
+mlflow.search_runs(
+    experiment_names=["/Shared/lj_loan_prediction"],
+    filter_string="tags.branch='week2'",
+)
+
+# COMMAND ----------
+
 run_id = mlflow.search_runs(
-    experiment_names=["/Shared/house-prices"],
+    experiment_names=["/Shared/lj_loan_prediction"],
     filter_string="tags.branch='week2'",
 ).run_id[0]
 
@@ -41,7 +53,7 @@ model = mlflow.sklearn.load_model(f'runs:/{run_id}/lightgbm-pipeline-model')
 
 # COMMAND ----------
 
-class HousePriceModelWrapper(mlflow.pyfunc.PythonModel):
+class LoanPredictionModelWrapper(mlflow.pyfunc.PythonModel):
     
     def __init__(self, model):
         self.model = model
@@ -49,13 +61,13 @@ class HousePriceModelWrapper(mlflow.pyfunc.PythonModel):
     def predict(self, context, model_input):
         if isinstance(model_input, pd.DataFrame):
             predictions = self.model.predict(model_input)
-            predictions = {"Prediction": adjust_predictions(
-                predictions[0])}
+            predictions = {"Prediction": predictions[0]}
             return predictions
         else:
             raise ValueError("Input must be a pandas DataFrame.")
 
 # COMMAND ----------
+
 train_set = spark.table(f"{catalog_name}.{schema_name}.train_set")
 test_set = spark.table(f"{catalog_name}.{schema_name}.test_set")
 
@@ -66,17 +78,19 @@ X_test = test_set[num_features + cat_features].toPandas()
 y_test = test_set[[target]].toPandas()
 
 # COMMAND ----------
-wrapped_model = HousePriceModelWrapper(model) # we pass the loaded model to the wrapper
+
+wrapped_model = LoanPredictionModelWrapper(model) # we pass the loaded model to the wrapper
 example_input = X_test.iloc[0:1]  # Select the first row for prediction as example
 example_prediction = wrapped_model.predict(context=None, model_input=example_input)
 print("Example Prediction:", example_prediction)
 
 # COMMAND ----------
+
 # this is a trick with custom packages
 # https://docs.databricks.com/en/machine-learning/model-serving/private-libraries-model-serving.html
 # but does not work with pyspark, so we have a better option :-)
 
-mlflow.set_experiment(experiment_name="/Shared/house-prices-pyfunc")
+mlflow.set_experiment(experiment_name="/Shared/lj_loan_prediction-pyfunc")
 git_sha = "ffa63b430205ff7"
 
 with mlflow.start_run(tags={"branch": "week2",
@@ -89,34 +103,38 @@ with mlflow.start_run(tags={"branch": "week2",
     mlflow.log_input(dataset, context="training")
     conda_env = _mlflow_conda_env(
         additional_conda_deps=None,
-        additional_pip_deps=["code/housing_price-0.0.1-py3-none-any.whl",
+        additional_pip_deps=["/Volumes/heiaepgah71pwedmld01001/lj_loan_prediction/packages/mlops_with_databricks-0.0.1-py3-none-any.whl",
                              ],
         additional_conda_channels=None,
     )
     mlflow.pyfunc.log_model(
         python_model=wrapped_model,
-        artifact_path="pyfunc-house-price-model",
-        code_paths = ["../housing_price-0.0.1-py3-none-any.whl"],
+        artifact_path="pyfunc-model",
+        # code_paths = ["../mlops_with_databricks-0.0.1-py3-none-any.whl"],
         signature=signature
     )
 
 # COMMAND ----------
-loaded_model = mlflow.pyfunc.load_model(f'runs:/{run_id}/pyfunc-house-price-model')
+
+loaded_model = mlflow.pyfunc.load_model(f'runs:/{run_id}/pyfunc-model')
 loaded_model.unwrap_python_model()
 
 # COMMAND ----------
-model_name = f"{catalog_name}.{schema_name}.house_prices_model_pyfunc"
+
+model_name = f"{catalog_name}.{schema_name}.loan_prediction_model_pyfunc"
 
 model_version = mlflow.register_model(
-    model_uri=f'runs:/{run_id}/pyfunc-house-price-model',
+    model_uri=f'runs:/{run_id}/pyfunc-model',
     name=model_name,
     tags={"git_sha": f"{git_sha}"})
+
 # COMMAND ----------
 
 with open("model_version.json", "w") as json_file:
     json.dump(model_version.__dict__, json_file, indent=4)
 
 # COMMAND ----------
+
 model_version_alias = "the_best_model"
 client.set_registered_model_alias(model_name, model_version_alias, "1")  
  
@@ -124,6 +142,9 @@ model_uri = f"models:/{model_name}@{model_version_alias}"
 model = mlflow.pyfunc.load_model(model_uri)
 
 # COMMAND ----------
+
 client.get_model_version_by_alias(model_name, model_version_alias)
+
 # COMMAND ----------
+
 model
