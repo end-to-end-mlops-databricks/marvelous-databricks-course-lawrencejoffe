7ed1eade09a84735451de6963f70beee6534b955
Lawrence Joffe <lawrence.joffe@heineken.com>
2024-12-12 00:43:46 +1100

    feature serving working

--- notebooks/week3/01.feature_serving.py
+++ notebooks/week3/01.feature_serving.py
@@ -1,5 +1,9 @@
 # Databricks notebook source
-# MAGIC %pip install ../housing_price-0.0.1-py3-none-any.whl
+# MAGIC %pip install ../mlops_with_databricks-0.0.1.tar.gz
+
+# COMMAND ----------
+
+# MAGIC %pip install --upgrade databricks-sdk
 
 # COMMAND ----------
 
@@ -35,7 +39,7 @@ from databricks.sdk.service.catalog import (
 from databricks.sdk.service.serving import EndpointCoreConfigInput, ServedEntityInput
 from pyspark.sql import SparkSession
 
-from house_price.config import ProjectConfig
+from loan_prediction.config import ProjectConfig
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -54,7 +58,7 @@ mlflow.set_registry_uri("databricks-uc")
 # COMMAND ----------
 
 # Load config
-config = ProjectConfig.from_yaml(config_path="../../project_config.yml")
+config = ProjectConfig.from_yaml(config_path="../../config/config.yml")
 
 # Get feature columns details
 num_features = config.num_features
@@ -64,8 +68,8 @@ catalog_name = config.catalog_name
 schema_name = config.schema_name
 
 # Define table names
-feature_table_name = f"{catalog_name}.{schema_name}.house_prices_preds"
-online_table_name = f"{catalog_name}.{schema_name}.house_prices_preds_online"
+feature_table_name = f"{catalog_name}.{schema_name}.loan_prediction_preds"
+online_table_name = f"{catalog_name}.{schema_name}.loan_prediction_preds_online_table"
 
 # Load training and test sets from Catalog
 train_set = spark.table(f"{catalog_name}.{schema_name}.train_set").toPandas()
@@ -75,26 +79,55 @@ df = pd.concat([train_set, test_set])
 
 # COMMAND ----------
 
+df.head(5).T
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ## Load a registered model
 
 # COMMAND ----------
 
 # Load the MLflow model for predictions
-pipeline = mlflow.sklearn.load_model(f"models:/{catalog_name}.{schema_name}.house_prices_model/2")
+pipeline = mlflow.pyfunc.load_model(f"models:/{catalog_name}.{schema_name}.loan_prediction_model_pyfunc/2")
+
+# pipeline = mlflow.pyfunc.load_model(f"models:/{catalog_name}.{schema_name}.lj_loan_prediction_model_fe/1")
+
+# COMMAND ----------
+
+df.columns
+
+# COMMAND ----------
+
+feature_columns = ["id","loan_percent_income"]
 
 # COMMAND ----------
 
 # Prepare the DataFrame for predictions and feature table creation - these features are the ones we want to serve.
-preds_df = df[["Id", "GrLivArea", "YearBuilt"]]
-preds_df["Predicted_SalePrice"] = pipeline.predict(df[cat_features + num_features])
+preds_df = df[feature_columns]
+preds_df["predicted_loan_status"] = pipeline.predict(df[cat_features + num_features])
 
 preds_df = spark.createDataFrame(preds_df)
+
+
+# COMMAND ----------
+
+preds_df
+
+# COMMAND ----------
+
+feature_table_name
+
+# COMMAND ----------
+
 
 # 1. Create the feature table in Databricks
 
 fe.create_table(
-    name=feature_table_name, primary_keys=["Id"], df=preds_df, description="House Prices predictions feature table"
+    name=feature_table_name, 
+    primary_keys=["id"], 
+    df=preds_df, 
+    description="Loan status predictions feature table"
 )
 
 # Enable Change Data Feed
@@ -105,32 +138,69 @@ spark.sql(f"""
 
 # COMMAND ----------
 
+online_table_name
+
+# COMMAND ----------
+
+from pprint import pprint
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.catalog import *
+import mlflow
+
+# COMMAND ----------
+
+# breaking changes to API in databricks-sdk > 0.20. Refer to https://docs.databricks.com/en/machine-learning/feature-store/online-tables.html for new syntax for create and wait
+
+# COMMAND ----------
+
 # 2. Create the online table using feature table
 
 spec = OnlineTableSpec(
-    primary_key_columns=["Id"],
+    primary_key_columns=["id"],
     source_table_full_name=feature_table_name,
     run_triggered=OnlineTableSpecTriggeredSchedulingPolicy.from_dict({"triggered": "true"}),
-    perform_full_copy=False,
+    perform_full_copy=False
+)
+
+online_table = OnlineTable(
+  name=online_table_name,
+  spec=spec
 )
 
 # Create the online table in Databricks
-online_table_pipeline = workspace.online_tables.create(name=online_table_name, spec=spec)
+online_table_pipeline = workspace.online_tables.create_and_wait(table=online_table)
+
+
 
 # COMMAND ----------
+
+## explanation for below creating a feature spec
+# ### https://docs.databricks.com/en/machine-learning/feature-store/online-tables.html
+#When you create a feature spec, you specify the source Delta table. This allows the feature spec to be used in #both offline and online scenarios. For online lookups, the serving endpoint automatically uses the online #table to perform low-latency feature lookups.
+
+
+
+# COMMAND ----------
+
+#fe.delete_feature_spec(name=feature_spec_name)
+
+# COMMAND ----------
+
 # 3. Create feture look up and feature spec table feature table
 
 # Define features to look up from the feature table
 features = [
     FeatureLookup(
-        table_name=feature_table_name, lookup_key="Id", feature_names=["GrLivArea", "YearBuilt", "Predicted_SalePrice"]
+        table_name=feature_table_name, lookup_key="id", feature_names=["loan_percent_income", "predicted_loan_status"]
     )
 ]
 
 # Create the feature spec for serving
-feature_spec_name = f"{catalog_name}.{schema_name}.return_predictions"
+feature_spec_name = f"{catalog_name}.{schema_name}.return_predictions_feature_spec"
 
 fe.create_feature_spec(name=feature_spec_name, features=features, exclude_columns=None)
+
+
 
 # COMMAND ----------
 
@@ -138,11 +208,17 @@ fe.create_feature_spec(name=feature_spec_name, features=features, exclude_column
 # MAGIC ## Deploy Feature Serving Endpoint
 
 # COMMAND ----------
+
 # 4. Create endpoing using feature spec
 
 # Create a serving endpoint for the house prices predictions
-workspace.serving_endpoints.create(
-    name="house-prices-feature-serving",
+
+
+endpoint_name = 'lj-loan-prediction-feature-serving'
+
+#updated api
+workspace.serving_endpoints.create_and_wait( 
+    name=endpoint_name,
     config=EndpointCoreConfigInput(
         served_entities=[
             ServedEntityInput(
@@ -154,12 +230,16 @@ workspace.serving_endpoints.create(
     ),
 )
 
+
+
+
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## Call The Endpoint
 
 # COMMAND ----------
+
 
 
 # COMMAND ----------
@@ -169,18 +249,16 @@ host = spark.conf.get("spark.databricks.workspaceUrl")
 
 # COMMAND ----------
 
-id_list = preds_df["Id"]
-
-# COMMAND ----------
+id_list = preds_df["id"]
 
 # COMMAND ----------
 
 start_time = time.time()
-serving_endpoint = f"https://{host}/serving-endpoints/house-prices-feature-serving/invocations"
+serving_endpoint = f"https://{host}/serving-endpoints/lj-loan-prediction-feature-serving/invocations"
 response = requests.post(
     f"{serving_endpoint}",
     headers={"Authorization": f"Bearer {token}"},
-    json={"dataframe_records": [{"Id": "182"}]},
+    json={"dataframe_records": [{"id": "10"}]},
 )
 
 end_time = time.time()
@@ -192,21 +270,29 @@ print("Execution time:", execution_time, "seconds")
 
 
 # COMMAND ----------
+
 # another way to call the endpoint
 
 response = requests.post(
     f"{serving_endpoint}",
     headers={"Authorization": f"Bearer {token}"},
-    json={"dataframe_split": {"columns": ["Id"], "data": [["182"]]}},
+    json={"dataframe_split": {"columns": ["id"], "data": [["10"]]}},
 )
+
+print("Response status:", response.status_code)
+print("Reponse text:", response.text)
+print("Execution time:", execution_time, "seconds")
+
+# COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## Load Test
 
 # COMMAND ----------
+
 # Initialize variables
-serving_endpoint = f"https://{host}/serving-endpoints/house-prices-feature-serving/invocations"
-id_list = preds_df.select("Id").rdd.flatMap(lambda x: x).collect()
+serving_endpoint = f"https://{host}/serving-endpoints/lj-loan-prediction-feature-serving/invocations"
+id_list = preds_df.select("id").rdd.flatMap(lambda x: x).collect()
 headers = {"Authorization": f"Bearer {token}"}
 num_requests = 10
 
@@ -218,7 +304,7 @@ def send_request():
     response = requests.post(
         serving_endpoint,
         headers=headers,
-        json={"dataframe_records": [{"Id": random_id}]},
+        json={"dataframe_records": [{"id": random_id}]},
     )
     end_time = time.time()
     latency = end_time - start_time  # Calculate latency for this request
